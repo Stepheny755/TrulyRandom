14b26b23bfb5aee100d88e9946ef222b3cd6def2
Stephen Yang <stepheny755@gmail.com>
2018-12-03 11:44:49 -0800

    Update rng.cpp

--- rng.cpp
+++ rng.cpp
@@ -2,3 +2,11 @@
 #include <time.h>
 
 #include "rng.h"
+
+const float currentTime(){
+  time_t timenow = time(0); 
+  struct tm tstruct = *localtime(&timenow);
+  
+  float f = tstruct.tm_min + tstruct.tm_sec/60;
+}
+
