694dfe63b6c4c24bf9059b1c12d197519aab247d
Stephen Yang <stepheny755@gmail.com>
2018-12-04 17:41:06 -0800

    Update rng.cpp

--- rng.cpp
+++ rng.cpp
@@ -13,6 +13,25 @@ float currentTime(){
 
 TRNG::TRNG(){
   
-  std::cout << currentTime();
+  //std::cout << currentTime();
+  for(int i = 0; i < 10 ; i++) {
+
+        int first_clock = clock();
+        int first_time = time(NULL);
+
+        while(time(NULL) <= first_time) {}
+
+        int second_time = time(NULL);
+        int second_clock = clock();
+
+        cout << "Actual clocks per second = " << (second_clock - first_clock)/(second_time - first_time) << "\n";
+
+        cout << "CLOCKS_PER_SEC = " << CLOCKS_PER_SEC << "\n";
+
+    }
+
+    return 0;
+  
+  
   
 }
